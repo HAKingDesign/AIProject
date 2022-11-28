a5245f0ff65ca3d25c2f0694ad41244c8e8ff3f6
baglobaglo <baglosebastian@gmail.com>
2022-11-28 15:47:11 -0600

    styleguide for the two classes done

--- Assets/MusicControlScript.cs
+++ Assets/MusicControlScript.cs
@@ -1,10 +1,37 @@
+/*
+
+    AUTHOR: Sebastian Baglo
+    FILENAME: MusicControlScript.cs
+    SPECIFICATION: Control and manage music in-game
+    FOR: CS3368 Introducation to Artifical Intelligence Section 001
+
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+
+    NAME: MusicControlScript
+    PURPOSE: Manage the in-game music playing when starting a game
+    INVARIANTS: When the music is done it should replay
+
+*/
+
 public class MusicControlScript : MonoBehaviour
 {
-    public static MusicControlScript instance;
+    public static MusicControlScript instance; //Make the sound an instance so we can accsess it
+
+    /*
+
+    NAME: Awake
+    PARAMETERS: None
+    PURPOSE: Initiate the music playing when game starts.
+    PRECONDITION: Must start a game for it too trigger
+    POSTCONDITION: Music loops
+
+    */
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
